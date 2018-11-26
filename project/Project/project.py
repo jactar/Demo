5b43ece26b32496177b6f88b3ec2a1704475138c
Jacob Tarango <Bernie@Bernies-MacBook-Air.local>
2018-11-26 14:07:20 -0800

    third push

--- project/Project/project.py
+++ project/Project/project.py
@@ -1,39 +1,45 @@
-# This file was created by Jacob Tarango
-# Thanks Chris Bradfeild from Kids Can Code
+#This file was created by Ricardo Pedrayes And Jacob Tarango
 
-import pygame as pg
-from pygame.sprite import Sprite
-import random
 from settings import *
-from test import *
+from sprites1 import *
+import random
+import pygame as pg
 
 class Game:
     def __init__(self):
         # init game window, try:
         pg.init()
         pg.mixer.init()
-        self.screen = pg.display.set_mode((WIDTH,HEIGHT))
-        pg.display.set_caption("jumpy_zzn")
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
-        # self.gravity
-        # init pygame and create ...
+        # init pygame and create...
     def new(self):
         self.all_sprites = pg.sprite.Group()
-        self.player = Player()
-        self.all_sprites.add(self.player)
+        self.player_1 = Player()
+        self.player_2 = Player()
+        # Player 2 controls and other info
+        self.player_2.up = pg.K_w
+        self.player_2.down = pg.K_s
+        self.player_2.rect.center = (WIDTH/12, HEIGHT/2)
+
+        self.all_sprites.add(self.player_1)
+        self.all_sprites.add(self.player_2)
         self.run()
         # create new player object
     def run(self):
         self.playing = True
+        # game loop
         while self.playing:
             self.clock.tick(FPS)
             self.events()
             self.update()
             self.draw()
-        # game loop
+
     def update(self):
         self.all_sprites.update()
+
         # update things
     def events(self):
         for event in pg.event.get():
@@ -43,21 +49,23 @@ class Game:
                 self.running = False
         # listening for events
     def draw(self):
-        self.screen.fill(REDDISH)
+        self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
-        # double buffer
+        self.screen.line(self.image, WHITE, (0,0), (0,5,), width=1)
+        #double buffer
         pg.display.flip()
-        
+
     def show_start_screen(self):
         pass
+
     def show_go_screen(self):
         pass
-    
 
 g = Game()
-g.show_go_screen()
+g.show_start_screen()
+
 while g.running:
     g.new()
     g.show_go_screen()
 
-g.QUIT
+pg.quit()
